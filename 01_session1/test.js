999fb56685ddd731448d6e7664c57f6c474ba772
Sparsh9844 <rajputsparsh570@gmail.com>
2025-09-14 07:05:40 +0000

    variables

--- 01_session1/test.js
+++ 01_session1/test.js
@@ -1,1 +1,10 @@
 console.log("sparsh")
+
+//GET STARTED WITH VARIABLES
+// prefer not to use var because of functional scope and block scope
+
+const accountId = 12345
+let accountName = "Sparsh"
+const accountPaswd = "@1234"
+
+console.table([accountId, accountName, accountPaswd]) //we dont need to randomly access the variables rather we can use "table"
